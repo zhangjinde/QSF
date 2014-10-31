d0f63c3a781238f94d091e2bb96b3229b36294e1
ichenq <ichenq@gmail.com>
2014-10-31 22:48:00 +0800

    utils.random

--- src/lualib-src/lua_init.cpp
+++ src/lualib-src/lua_init.cpp
@@ -1,18 +1,21 @@
 #include "lua_init.h"
 #include <lua.hpp>
 
+
+// declarations
 extern "C" {
 
 int luaopen_qsf_c(lua_State* L);
+int luaopen_utils(lua_State* L);
 
 }
-
 
 void lua_initlibs(lua_State* L)
 {
     static const luaL_Reg libs[] =
     {
         { "qsf.c", luaopen_qsf_c },
+        { "utils", luaopen_utils },
         { NULL, NULL },
     };
 
---
+++ src/lualib-src/lua_utils.cpp
@@ -0,0 +1,47 @@
+#include <thread>
+#include <lua.hpp>
+#include "core/random.h"
+
+
+// a drop-in replacement for `math.random`
+static int utils_random(lua_State *L)
+{
+    switch (lua_gettop(L))   /* check number of arguments */
+    {
+    case 0:   /* no arguments */
+    {
+        lua_pushnumber(L, Random::randDouble01());  /* Number between 0 and 1 */
+        break;
+    }
+    case 1:   /* only upper limit */
+    {
+        lua_Integer upper = luaL_checkinteger(L, 1);
+        luaL_argcheck(L, 1 <= upper, 1, "interval is empty");
+        lua_pushinteger(L, Random::rand64(upper + 1));  /* [1, u] */
+        break;
+    }
+    case 2:   /* lower and upper limits */
+    {
+        lua_Integer lower = luaL_checkinteger(L, 1);
+        lua_Integer upper = luaL_checkinteger(L, 2);
+        luaL_argcheck(L, lower <= upper, 2, "interval is empty");
+        lua_pushinteger(L, Random::rand64(lower, upper + 1));  /* [l, u] */
+        break;
+    }
+    default: 
+        return luaL_error(L, "wrong number of arguments");
+    }
+    return 1;
+}
+
+extern "C"
+int luaopen_utils(lua_State* L)
+{
+    static const luaL_Reg lib[] =
+    {        
+        { "random", utils_random },
+        { NULL, NULL },
+    };
+    luaL_newlib(L, lib);
+    return 1;
+}
