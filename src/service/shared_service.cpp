6e7a14b155070aefe8acea80b048d7f8797b3dca
ichenq <ichenq@gmail.com>
2014-12-20 21:28:28 +0800

    copyright notice

--- src/service/shared_service.cpp
+++ src/service/shared_service.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2014 ichenq@gmail.com. All rights reserved.
+// Distributed under the terms and conditions of the Apache License.
+// See accompanying files LICENSE.
+
 #include "shared_service.h"
 #include "core/conv.h"
 #include "core/strings.h"
