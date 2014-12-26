49448b523f80e0f69a789e0e771a8bf1a6170aa1
ichenq <ichenq@gmail.com>
2014-12-27 05:36:05 +0800

    fix compression test case

--- src/net/test/compression_test.cpp
+++ src/net/test/compression_test.cpp
@@ -13,11 +13,12 @@ TEST(compression, compressServerPacket)
     {
         auto str = randString(size);
         ByteRange origin = StringPiece(str);
-        auto buf = compressServerPacket(NO_COMPRESSION, origin, false);
+        CodecType codec = size > 128 ? ZLIB : NO_COMPRESSION;
+        auto buf = compressServerPacket(codec, origin, false);
         EXPECT_FALSE(buf->empty());
         const ServerHeader* head = reinterpret_cast<const ServerHeader*>(buf->buffer());
         const size_t head_size = sizeof(*head);
-        EXPECT_EQ(head->size, origin.size());
+        EXPECT_EQ(head->codec, codec);
         auto compressed_frame = ByteRange(buf->buffer() + head_size, buf->length()-head_size);
         auto buf_out = uncompressPacketFrame((CodecType)head->codec, compressed_frame);
         EXPECT_EQ(origin, buf_out->byteRange());
@@ -29,12 +30,14 @@ TEST(compression, compressClientPacket)
     for (auto size : { 32, 64, 128, 512, 1024, 2048, 8196 })
     {
         auto str = randString(size);
+        const CodecType codec = NO_COMPRESSION;
         ByteRange origin = StringPiece(str);
-        auto buf = compressClientPacket(NO_COMPRESSION, origin);
+        auto buf = compressClientPacket(codec, origin);
         EXPECT_FALSE(buf->empty());
         const ClientHeader* head = reinterpret_cast<const ClientHeader*>(buf->buffer());
         const size_t head_size = sizeof(*head);
-        auto buf_out = uncompressPacketFrame(NO_COMPRESSION, buf->byteRange());
+        auto compressed_frame = ByteRange(buf->buffer() + head_size, buf->length() - head_size);
+        auto buf_out = uncompressPacketFrame(codec, compressed_frame);
         EXPECT_EQ(origin, buf_out->byteRange());
     }
 }
