9a84b146f9b71c8071c8697e97d114742b00e259
ichenq <ichenq@gmail.com>
2015-03-08 12:48:55 +0800

    remove lua_init.h

--- src/lualib/lua_init.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2014 ichenq@gmail.com. All rights reserved.
-// Distributed under the terms and conditions of the Apache License.
-// See accompanying files LICENSE.
-
-#pragma once
-
-struct lua_State;
-
-void lua_initlibs(lua_State* L);
