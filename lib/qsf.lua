5ae03ccfa7f1a3901b390a55d97ef3c3f2018746
ichenq <ichenq@gmail.com>
2015-03-19 13:48:16 +0800

    qsf module

--- lib/qsf.lua
+++ lib/qsf.lua
@@ -1,149 +1,68 @@
+--
+-- Dispatch service message
+--
 local mq = require 'mq'
 local process = require 'process'
-local cmsgpack = require 'cmsgpack'
+local mp = require 'MessagePack'
 local trace = require 'trace'
+local dumpstring = require 'dump'.dumpstring -- debugging only
 
-local type = type
-local pcall = pcall
-local print = print
-local error = error
-local assert = assert
-local table = table
-local coroutine = coroutine
+local xpcall, tunpack, tpack = xpcall
+local mq_send, mq_recv, mp_pack = mq.send, mq.recv, mp.pack
+local process_gettick = process.gettick
+local process_sleep = process.sleep
+local trace_stack = trace.dump_stack
 
 local qsf = {}
 
-local MIN_FRAME_TIME = 10 --ms
-
-local session_id = 0
-local session_id_coroutine = {}
-local coroutine_session_id = {}
-
--- remote procedure call, current coroutine will be suspend
-function qsf.call(node, method, ...)
-    local session_id = coroutine_session_id[coroutine.running()]
-    local request = {method=method, id=session_id, params={...}}
-    mq.send(node, cmsgpack.pack(request))
-    return coroutine.yield()
-end
-
 -- send notify message
 function qsf.notify(node, method, ...)
-    local request = {method=method, params={...}}
-    mq.send(node, cmsgpack.pack(request))
+    mq_send(node, mp_pack{method=method, params={...}})
 end
 
+-- launch new service
 function qsf.launch(name, path, ...)
-    mq.launch('LuaService', name, path, ...)
+    mq.launch(name, path, ...)
 end
 
-function qsf.launch_native(name, path, ...)
-    mq.launch('SharedService', name, path, ...)
+-- send log text to logger service
+function qsf.log(...)
+    qsf.notify('logger', 'print', ...)
 end
 
--- reuse coroutine
-local coroutine_pool = {}
-local coroutine_count = 0
-
-local function co_create(f)
-    local co = table.remove(coroutine_pool)
-    if co == nil then
-        co = coroutine.create(function(...)
-            f(...)
-            while true do
-                f = nil
-                coroutine_pool[#coroutine_pool+1] = co
-                f = coroutine.yield('EXIT')
-                f(coroutine.yield())
+local function dispatch_message(service, idle)
+    local opt = (func ~= nil and 'nowait' or nil)
+    local from, data = mq_recv(opt)
+    if from and data then
+        --print(from .. ' ==> ' .. mq.name(), data)
+        local request = mp_unpack(data) -- JSON RPC variant
+        local func = service[msg.method]
+        if func then 
+            local result = {func(tunpack(msg.params))}
+            if #result > 0 then 
+                local response = mp_pack{method=msg.method, params=result}
+                mq_send(from, response)
             end
-        end)
-        coroutine_count = coroutine_count + 1
-        if coroutine_count > 1024 then
-            error("coroutine more than 1024 may overload")
-            coroutine_count = 0
         end
     else
-        coroutine.resume(co, f)
-    end
-    return co
-end
-
-local function suspend(co, ok, command)
-    local session = coroutine_session_id[co]
-    if not ok then
-        coroutine_session_id[co] = nil
-        session_id_coroutine[session_id] = nil
-        error('session not found')
-    end
-    if command == 'EXIT' then
-        coroutine_session_id[co] = nil
-        session_id_coroutine[session_id] = nil        
-    end
-end
-
--- dispatch inproc message
-local function dispatch_rpc_message(from, data, service)
-    local msg = cmsgpack.unpack(data)
-    if msg.method then --request from other service
-        session_id = session_id + 1
-        local co = co_create(function ()
-            local func = service[msg.method]
-            if func then
-                local result = {func(table.unpack(msg.params))}
-                if #result > 0 then
-                    mq.send(from, cmsgpack.pack({id=session_id, result=result}))
-                end
-            else
-                mq.send(from, cmsgpack.pack({id=session_id, error='method not found'}))
-            end
-        end)
-        coroutine_session_id[co] = session_id
-        session_id_coroutine[session_id] = co
-        suspend(co, coroutine.resume(co))
-    else --response for this service
-        local co = session_id_coroutine[msg.id]
-        if co then
-            coroutine.resume(co, table.unpack(msg.result))
-        else
-            print('unknown response:', msg.id)
+        if idle then 
+            idle() -- dispatch user-defined function
         end
     end
-end
-
-local function dispatch_message(service, func)
-    local opt = (func ~= nil and 'nowait' or nil)
-    local from, data = mq.recv(opt)
-    if from and data then
-        -- exit signal
-        if from == 'sys' and data == 'exit' then
-            mq.send(from, 'roger')
-            return false
-        else
-            dispatch_rpc_message(from, data, service)
-        end
-        return true
-    end
-    -- dispatch user-defined function
-    if func then
-        func()
-    end
-    return true
 end
 
 function qsf.run(service, func)
     assert(func == nil or type(func) == 'function')
+    trace.dumper = qsf.log
     while true do
-        local tick_start = process.gettick()
-        local ok, result = xpcall(dispatch_message, trace.dump_stack, service, func)
+        local ticks = process_gettick()
+        local ok, result = xpcall(dispatch_message, trace_stack, service, func)
         if ok then 
             if not result then break end
-        else
-            print(result) -- error message
         end
-        local tick_end = process.gettick()
-        local tick_frame = tick_start - tick_end
-        if tick_frame < MIN_FRAME_TIME then
-            process.sleep(3000) -- yield OS thread context
+        ticks = process_gettick() - ticks
+        if ticks < 10 then
+            process_sleep(5) -- yield OS thread context
         end
     end
 end
