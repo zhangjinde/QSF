f7b003312c03f5060c475832f4e57ca977c3ec13
ichenq <ichenq@gmail.com>
2015-04-09 18:36:52 +0800

    Add qsf_realloc

--- src/qsf_malloc.h
+++ src/qsf_malloc.h
@@ -4,12 +4,14 @@
 
 #pragma once
 
-#include <stdlib.h>
+#include <malloc.h>
 
 #ifdef USE_JEMALLOC
 # define qsf_malloc     je_malloc
 # define qsf_free       je_free
+# define qsf_realloc    je_realloc
 #else
-# define qsf_malloc      malloc
-# define qsf_free        free
+# define qsf_malloc     malloc
+# define qsf_free       free
+# define qsf_realloc    realloc
 #endif
