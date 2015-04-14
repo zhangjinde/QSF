3c5b8775c0df13587c0e10e806ea21d223b0ddb9
ichenq <ichenq@gmail.com>
2015-04-14 19:41:51 +0800

    adapt jemalloc in linkage

--- src/qsf_malloc.h
+++ src/qsf_malloc.h
@@ -4,18 +4,14 @@
 
 #pragma once
 
+#include <malloc.h>
+
+// http://www.canonware.com/download/jemalloc/jemalloc-latest/doc/jemalloc.html
 #ifdef USE_JEMALLOC
 # include <jemalloc/jemalloc.h>
-# define qsf_malloc     je_malloc
-# define qsf_free       je_free
-# define qsf_realloc    je_realloc
-#else 
-# ifdef __APPLE__
-#  include <malloc/malloc.h>
-# else
-#  include <malloc.h>
-# endif
-# define qsf_malloc     malloc
-# define qsf_free       free
-# define qsf_realloc    realloc
-#endif 
+#endif
+
+// `malloc` functions are defined as a weak symbols in glibc.
+#define qsf_malloc     malloc
+#define qsf_free       free
+#define qsf_realloc    realloc
