7b39f77fb7287477e02d75083812f825d16797a0
ichenq <ichenq@gmail.com>
2015-04-16 16:08:03 +0800

    lua-msgpack: fix packing sparse array

--- src/lualib/lua_msgpack.c
+++ src/lualib/lua_msgpack.c
@@ -4,49 +4,78 @@
 
 #include <assert.h>
 #include <stdint.h>
+#include <math.h>
 #include <float.h>
 #include <lua.h>
 #include <lauxlib.h>
+
+#define MSGPACK_SBUFFER_INIT_SIZE   1024
 #include <msgpack.h>
 
-#define SHORT_STR_SIZE    256
+#define MP_SHORT_STR_SIZE   256
+#define MP_MAX(a, b)        ((a) > (b) ? (a) : (b))
+
+#define MP_PACK_FINAL(pk)   msgpack_sbuffer_destroy((msgpack_sbuffer*)(pk)->data)
+
+#define MP_VALIDATE_NUMBER(pk, v) \
+    if (mp_config.validate_number && (isinf(val) || isnan(val))) \
+    { \
+        MP_PACK_FINAL(pk);\
+        luaL_error(L, "number is NaN or Infinity");\
+    }
+
+typedef struct mp_config_s
+{
+    int sparse_array_as_map;
+    int validate_number;
+    int encode_str_bin;
+}mp_config_t;
+
+static mp_config_t mp_config;
 
 // pack Lua value to msgpack format
 static void mp_pack_value(lua_State* L, msgpack_packer* packer);
-static void mp_pack_array(lua_State* L, msgpack_packer* packer);
+static void mp_pack_array(lua_State* L, msgpack_packer* packer, int size);
 static void mp_pack_map(lua_State* L, msgpack_packer* packer);
 
 // unpack msgpack data to Lua value
-static void mp_unpack_value(lua_State* L, const msgpack_object* value);
-static void mp_unpack_array(lua_State* L, const msgpack_object* value);
-static void mp_unpack_map(lua_State* L, const msgpack_object* value);
+static int mp_unpack_value(lua_State* L, const msgpack_object* value);
+static int mp_unpack_array(lua_State* L, const msgpack_object* value);
+static int mp_unpack_map(lua_State* L, const msgpack_object* value);
 
-// is this table an array or hash
-static bool table_is_array(lua_State* L)
+static int mp_array_size(lua_State* L)
 {
     assert(lua_type(L, -1) == LUA_TTABLE);
-    int len = 0;
+    int k = 0, max = 0, items = 0;
     lua_pushnil(L);
     while (lua_next(L, -2) != 0)
     {
         if (lua_isinteger(L, -2))
         {
-            if (lua_tointeger(L, -2) == ++len)
+            k = (int)lua_tointeger(L, -2);
+            if (k >= 1)
             {
+                max = MP_MAX(k, max);
+                items++;
                 lua_pop(L, 1);
                 continue;
             }
         }
+        /* Must not be an array (non integer key) */
         lua_pop(L, 2);
-        return false;
+        return -1;
     }
-    return true;
+    if (mp_config.sparse_array_as_map && max > items)
+    {
+        return -1;
+    }
+    return max;
 }
 
-static size_t table_size(lua_State* L)
+static int mp_table_size(lua_State* L)
 {
     assert(lua_type(L, -1) == LUA_TTABLE);
-    size_t len = 0;
+    int len = 0;
     lua_pushnil(L);
     while (lua_next(L, -2) != 0)
     {
@@ -67,20 +96,17 @@ static void mp_pack_number(lua_State* L, msgpack_packer* packer)
     else
     {
         double val = luaL_checknumber(L, -1);
-        if (val < FLT_MAX && val > FLT_MIN)
-            msgpack_pack_float(packer, (float)val);
-        else
-            msgpack_pack_double(packer, val);
+        MP_VALIDATE_NUMBER(packer, v);
+        msgpack_pack_double(packer, val);
     }
 }
 
 // pack Lua array table
-static void mp_pack_array(lua_State* L, msgpack_packer* packer)
+static void mp_pack_array(lua_State* L, msgpack_packer* packer, int size)
 {
     assert(L && packer && lua_type(L, -1) == LUA_TTABLE);
-    size_t len = lua_rawlen(L, -1);
-    msgpack_pack_array(packer, len);
-    for (size_t i = 1; i <= len; i++)
+    msgpack_pack_array(packer, size);
+    for (size_t i = 1; i <= size; i++)
     {
         lua_rawgeti(L, -1, i);
         mp_pack_value(L, packer);
@@ -88,21 +114,100 @@ static void mp_pack_array(lua_State* L, msgpack_packer* packer)
     }
 }
 
+// unpack msgpack array to Lua table
+static int mp_unpack_array(lua_State* L, const msgpack_object* value)
+{
+    assert(L && value && value->type == MSGPACK_OBJECT_ARRAY);
+    const msgpack_object_array* array = &value->via.array;
+    lua_createtable(L, array->size, 0);
+    int index = 1;
+    for (uint32_t i = 0; i < array->size; i++)
+    {
+        if (mp_unpack_value(L, &array->ptr[i]) == 0)
+        {
+            lua_rawseti(L, -2, index++);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
 // pack Lua table, with hash part
 static void mp_pack_map(lua_State* L, msgpack_packer* packer)
 {
     assert(L && packer && lua_type(L, -1) == LUA_TTABLE);
-    msgpack_pack_map(packer, table_size(L));
+    msgpack_pack_map(packer, mp_table_size(L));
     lua_pushnil(L);
     while (lua_next(L, -2) != 0)
     {
-        size_t len = 0;
-        const char* key = luaL_checklstring(L, -2, &len);
-        msgpack_pack_str(packer, len);
-        msgpack_pack_str_body(packer, key, len);
+        int t = lua_type(L, -2);
+        switch (t)
+        {
+        case LUA_TNUMBER:
+            mp_pack_number(L, packer);
+            break;
+        case LUA_TSTRING:
+            {
+                size_t len = 0;
+                const char* key = luaL_checklstring(L, -2, &len);
+                msgpack_pack_str(packer, len);
+                msgpack_pack_str_body(packer, key, len);
+            }
+            break;
+        default:
+            MP_PACK_FINAL(packer);
+            luaL_error(L, "invalid table key type: %s", lua_typename(L, t));
+            break;
+        }
         mp_pack_value(L, packer);
         lua_pop(L, 1);
     }
+}
+
+// `k` for lua_setfield must be a null-terminated string
+inline void mp_set_map_key(lua_State* L, const msgpack_object_str* key)
+{
+    char short_str[MP_SHORT_STR_SIZE];
+    if (key->size < sizeof(short_str))
+    {
+        strncpy(short_str, key->ptr, key->size);
+        short_str[key->size] = '\0';
+        lua_setfield(L, -2, short_str);
+    }
+    else
+    {
+        char* buf = malloc(key->size + 1);
+        strncpy(buf, key->ptr, key->size);
+        buf[key->size] = '\0';
+        lua_setfield(L, -2, buf);
+        free(buf);
+    }
+}
+
+// unpack msgpack map(dictionary) to Lua table
+static int mp_unpack_map(lua_State* L, const msgpack_object* value)
+{
+    assert(L && value && value->type == MSGPACK_OBJECT_MAP);
+    const msgpack_object_map* map = &value->via.map;
+    lua_createtable(L, 0, map->size);
+    for (uint32_t i = 0; i < map->size; i++)
+    {
+        const msgpack_object_kv* item = &map->ptr[i];
+        const msgpack_object* key = &item->key;
+        assert(key->type == MSGPACK_OBJECT_STR);
+        if (mp_unpack_value(L, &item->val) == 0)
+        {
+            mp_set_map_key(L, &key->via.str);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 0;
 }
 
 // pack Lua value to msgpack format string
@@ -130,97 +235,44 @@ static void mp_pack_value(lua_State* L, msgpack_packer* packer)
         {
             size_t len = 0;
             const char* str = luaL_checklstring(L, -1, &len);
-            msgpack_pack_str(packer, len);
-            msgpack_pack_str_body(packer, str, len);
+            if (mp_config.encode_str_bin)
+            {
+                msgpack_pack_bin(packer, len);
+                msgpack_pack_bin_body(packer, str, len);
+            }
+            else
+            {
+                msgpack_pack_str(packer, len);
+                msgpack_pack_str_body(packer, str, len);
+            }
         }
         break;
 
     case LUA_TNUMBER:
-        {
-            mp_pack_number(L, packer);
-        }
+        mp_pack_number(L, packer);
         break;
 
     case LUA_TTABLE:
         {
-            if (table_is_array(L))
-                mp_pack_array(L, packer);
+            int size = mp_array_size(L);
+            if (size >= 0)
+            {
+                mp_pack_array(L, packer, size);
+            }
             else
+            {
                 mp_pack_map(L, packer);
+            }
         }
         break;
 
     default:
-        // `msgpack_sbuffer` is not freed, thus memory leak occurs.
+        MP_PACK_FINAL(packer);
         luaL_error(L, "cant encode value of type: %s", lua_typename(L, type)); 
     }
 }
 
-static int mp_pack(lua_State* L)
-{
-    msgpack_sbuffer* buf = msgpack_sbuffer_new();
-    msgpack_packer packer;
-    msgpack_packer_init(&packer, buf, msgpack_sbuffer_write);
-
-    // not exception-safe
-    mp_pack_value(L, &packer);
-    lua_pushlstring(L, buf->data, buf->size);
-
-    msgpack_sbuffer_free(buf);
-    return 1;
-}
-
-// unpack msgpack array to Lua table
-static void mp_unpack_array(lua_State* L, const msgpack_object* value)
-{
-    assert(L && value && value->type == MSGPACK_OBJECT_ARRAY);
-    const msgpack_object_array* array = &value->via.array;
-    lua_createtable(L, array->size, 0);
-    int index = 1;
-    for (uint32_t i = 0; i < array->size; i++)
-    {
-        mp_unpack_value(L, &array->ptr[i]);
-        lua_rawseti(L, -2, index++);
-    }
-}
-
-// `k` for lua_setfield must be a null-terminated string
-inline void mp_set_map_key(lua_State* L, const msgpack_object_str* key)
-{
-    char short_str[SHORT_STR_SIZE];
-    if (key->size < sizeof(short_str))
-    {
-        strncpy(short_str, key->ptr, key->size);
-        short_str[key->size] = '\0';
-        lua_setfield(L, -2, short_str);
-    }
-    else
-    {
-        char* buf = malloc(key->size + 1);
-        strncpy(buf, key->ptr, key->size);
-        buf[key->size] = '\0';
-        lua_setfield(L, -2, buf);
-        free(buf);
-    }
-}
-
-// unpack msgpack map(dictionary) to Lua table
-static void mp_unpack_map(lua_State* L, const msgpack_object* value)
-{
-    assert(L && value && value->type == MSGPACK_OBJECT_MAP);
-    const msgpack_object_map* map = &value->via.map;
-    lua_createtable(L, 0, map->size);
-    for (uint32_t i = 0; i < map->size; i++)
-    {
-        const msgpack_object_kv* item = &map->ptr[i];
-        const msgpack_object* key = &item->key;
-        assert(key->type == MSGPACK_OBJECT_STR);
-        mp_unpack_value(L, &item->val);
-        mp_set_map_key(L, &key->via.str);
-    }
-}
-
-static void mp_unpack_value(lua_State* L, const msgpack_object* value)
+static int mp_unpack_value(lua_State* L, const msgpack_object* value)
 {
     assert(L && value);
     switch (value->type)
@@ -272,17 +324,30 @@ static void mp_unpack_value(lua_State* L, const msgpack_object* value)
         break;
 
     case MSGPACK_OBJECT_ARRAY:
-        mp_unpack_array(L, value);;
-        break;
+        return mp_unpack_array(L, value);
 
     case MSGPACK_OBJECT_MAP:
-        mp_unpack_map(L, value);;
-        break;
+        return mp_unpack_map(L, value);
 
-    case MSGPACK_OBJECT_EXT:
-        // `msgpack_unpacked` object is not freed, thus memory leak occurs.
-        luaL_error(L, "unpack type of `ext` is not implemented");
+    default:
+        return -1;
     }
+    return 0;
+}
+
+static int mp_pack(lua_State* L)
+{
+    msgpack_sbuffer buf;
+    msgpack_sbuffer_init(&buf);
+    msgpack_packer packer;
+    msgpack_packer_init(&packer, &buf, msgpack_sbuffer_write);
+
+    // not exception-safe
+    mp_pack_value(L, &packer);
+    lua_pushlstring(L, buf.data, buf.size);
+
+    msgpack_sbuffer_destroy(&buf);
+    return 1;
 }
 
 static int mp_unpack(lua_State* L)
@@ -295,11 +360,9 @@ static int mp_unpack(lua_State* L)
     int err = msgpack_unpack_next(&msg, data, len, &offset);
     if (err == MSGPACK_UNPACK_SUCCESS)
     {
-        // not exception-safe
-        mp_unpack_value(L, &msg.data);
-
+        int r = mp_unpack_value(L, &msg.data);
         msgpack_unpacked_destroy(&msg);
-        return 1;
+        return (r == 0 ? 1 : 0);
     }
     else
     {
@@ -308,14 +371,35 @@ static int mp_unpack(lua_State* L)
     }
 }
 
+static int mp_set_option(lua_State* L)
+{
+    const char* option = luaL_checkstring(L, 1);
+    int enable = lua_toboolean(L, 2);
+    if (strcmp(option, "encode_array_as_map") == 0)
+    {
+        mp_config.sparse_array_as_map = enable;
+    }
+    else if (strcmp(option, "validate_number") == 0)
+    {
+        mp_config.validate_number = enable;
+    }
+    else if (strcmp(option, "encode_str_bin") == 0)
+    {
+        mp_config.encode_str_bin = enable;
+    }
+    return 0;
+}
+
 LUALIB_API int luaopen_msgpack(lua_State* L)
 {
     static const luaL_Reg lib[] =
     {
         { "pack", mp_pack },
         { "unpack", mp_unpack },
+        { "set_option", mp_set_option },
         { NULL, NULL },
     };
+    mp_config.validate_number = 1;
     luaL_newlib(L, lib);
     return 1;
 }
