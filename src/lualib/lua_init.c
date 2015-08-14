b5e483a8e67fc3154c00aec9232e9acc537a168b
ichenq <ichenq@gmail.com>
2015-08-14 16:34:35 +0800

    init preload module

--- src/lualib/lua_init.c
+++ src/lualib/lua_init.c
@@ -7,66 +7,41 @@
 #include "qsf.h"
 
 
-
-/* forward declarations */
-extern int luaopen_mq(lua_State* L);
-extern int luaopen_net(lua_State* L);
+// forward declarations
+extern int luaopen_node(lua_State* L);
 extern int luaopen_zmq(lua_State* L);
 extern int luaopen_process(lua_State* L);
-extern int luaopen_lfs(lua_State* L);
 extern int luaopen_zlib(lua_State* L); 
 extern int luaopen_mysql(lua_State* L);
-extern int luaopen_crypto(lua_State* L); 
+extern int luaopen_crypto(lua_State* L);
+extern int luaopen_net(lua_State* L);
+extern int luaopen_luv(lua_State *L);
+extern int luaopen_base64(lua_State* L);
+extern int luaopen_lfs(lua_State *L);
+
+static const luaL_Reg preload_libs[] =
+{
+    { "luv", luaopen_luv },
+    { "zmq", luaopen_zmq },
+    { "net", luaopen_net },
+    { "lfs", luaopen_lfs },
+    { "node", luaopen_node },
+    { "zlib", luaopen_zlib },
+    { "mysql", luaopen_mysql },
+    { "crypto", luaopen_crypto },
+    { "base64", luaopen_base64 },
+    { "process", luaopen_process },
+    { NULL, NULL },
+};
 
 void init_preload_libs(lua_State* L)
 {
-    static const luaL_Reg libs[] =
-    {
-        { "mq", luaopen_mq },
-        { "lfs", luaopen_lfs },
-        { "net", luaopen_net },
-        { "zmq", luaopen_zmq },
-        { "zlib", luaopen_zlib },
-        { "mysql", luaopen_mysql },
-        { "crypto", luaopen_crypto },
-        { "process", luaopen_process },
-        { NULL, NULL },
-    };
-
-    /* add open functions into 'package.preload' table */
+    // add open functions into 'package.preload' table
     luaL_getsubtable(L, LUA_REGISTRYINDEX, "_PRELOAD");
-    for (const luaL_Reg *lib = libs; lib->func; lib++)
+    for (const luaL_Reg *lib = preload_libs; lib->func; lib++)
     {
         lua_pushcfunction(L, lib->func);
         lua_setfield(L, -2, lib->name);
     }
-    lua_pop(L, 1);  /* remove _PRELOAD table */
-}
-
-static int ltraceback(lua_State* L)
-{
-    if (!lua_isstring(L, 1))   /* Non-string error object? Try metamethod. */
-    {
-        if (lua_isnoneornil(L, 1) ||
-            !luaL_callmeta(L, 1, "__tostring") ||
-            !lua_isstring(L, -1))
-            return 1;  /* Return non-string error object. */
-        lua_remove(L, 1);  /* Replace object by result of __tostring metamethod. */
-    }
-    luaL_traceback(L, L, lua_tostring(L, 1), 1);
-    return 1;
-}
-
-int lua_trace_call(lua_State* L, int narg)
-{
-    int base = lua_gettop(L) - narg;  /* function index */
-    lua_pushcfunction(L, ltraceback);  /* push traceback function */
-    lua_insert(L, base);  /* put it under chunk and args */
-    int r = lua_pcall(L, narg, 0, base);
-    lua_remove(L, base);  /* remove traceback function */
-    if (r != 0)
-    {
-        qsf_log("%s\n", lua_tostring(L, -1));
-    }
-    return r;
+    lua_pop(L, 1);  // remove _PRELOAD table
 }
